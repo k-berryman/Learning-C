69deaa93c860df2b387a66c0b71e7fa8b67be8bd
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2019-03-26 00:45:42 -0400

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,22 @@
+/* Bubble Sort
+ * Kaitlin Berryman */
+ #include <stdio.h>
+ 
+ void bubbleSort(int numbers[], int n)
+ {
+	for(int a=0; a<n-1; a++)
+		for(int b=0; b<n-1-a; b++)
+			if(numbers[b] > numbers[b+1]
+			{
+				int temp = numbers[b];
+				numbers[b] = numbers[b+1];
+				numbers[b+1] = temp;
+			}
+		}
+	}
+ }
+ 
+ int main()
+ {
+	 
+ }
